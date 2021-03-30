042f66dde691709a1e3d711008b3e119e33621e3
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-03-31 09:32:30 +1000

    Implement plotting of cones outside simulator for visualisation and problem solving

--- src/trajectory_finder/src/trajectory_finder.py
+++ src/trajectory_finder/src/trajectory_finder.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 import rospy
 from fssim_messages.msg import Map, Cone
+import matplotlib.pyplot as plt
+
+blue_x_coords = []
+blue_y_coords = []
+yellow_x_coords = []
+yellow_y_coords = []
 
 def callback(data):
 #    rospy.loginfo(rospy.get_caller_id() + " I heard %s", data.cones_blue[0].position.x)
-    rospy.loginfo(" I heard %s", data.cones_blue[0].position.x)
+    for blue_cones in data.cones_blue:
+        blue_x_coords.append(blue_cones.position.x)
+        blue_y_coords.append(blue_cones.position.y)
+
+    for yellow_cones in data.cones_yellow:
+        yellow_x_coords.append(yellow_cones.position.x)
+        yellow_y_coords.append(yellow_cones.position.y)
     
 def listener():
 
@@ -16,6 +28,12 @@ def listener():
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber("chicken/map", Map, callback)
+
+    plt.scatter(blue_x_coords, blue_y_coords, c='#0000ff')
+    plt.scatter(yellow_x_coords, yellow_y_coords, c='#ffff00')
+    plt.axes().set_aspect('equal')
+    plt.grid(True)
+    plt.show()
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
