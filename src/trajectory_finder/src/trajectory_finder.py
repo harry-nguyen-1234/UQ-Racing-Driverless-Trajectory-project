775f386fb0371bf2aff676f07f8fdef3fd0347f8
Harry Nguyen <huy.nguyen6@uqconnect.edu.au>
2021-04-02 15:33:05 +1000

    Publish trajectory to simulator

--- src/trajectory_finder/src/trajectory_finder.py
+++ src/trajectory_finder/src/trajectory_finder.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 import rospy
 from fssim_messages.msg import Map, Cone
+from geometry_msgs.msg import PolygonStamped, Point32
 import matplotlib.pyplot as plt
 from scipy.spatial import KDTree
 import numpy as np
+
+publisher = rospy.Publisher("chicken/trajectory", PolygonStamped, queue_size=1)
 
 def callback(data):
     blue_x_coords = []
     blue_y_coords = []
     yellow_x_coords = []
     yellow_y_coords = []
+    middle_points = []
+    trajectory_polygon = PolygonStamped()
 
     for blue_cones in data.cones_blue:
         blue_x_coords.append(blue_cones.position.x)
@@ -22,9 +27,9 @@ def callback(data):
     yellow_coords = zip(yellow_x_coords, yellow_y_coords)
 
     tree = KDTree(blue_coords)
-    indexes = tree.query(yellow_coords)[1]
+    indexes_of_closest = tree.query(yellow_coords)[1]
 
-    for count, value in enumerate(indexes):
+    for count, value in enumerate(indexes_of_closest):
         x = [
         blue_x_coords[value],
         yellow_x_coords[count]
@@ -34,29 +39,21 @@ def callback(data):
         blue_y_coords[value],
         yellow_y_coords[count]
         ]
+
         plt.plot(x, y, c='#ff0000')
         plt.scatter(np.mean(x), np.mean(y), c='#ff00ff')
 
-    # Draw the original raw data.
-    plt.scatter(blue_x_coords, blue_y_coords, c='#0000ff')
-    plt.scatter(yellow_x_coords, yellow_y_coords, c='#ffcc00')
-    plt.axes().set_aspect('equal')
-    plt.grid(True)
-    plt.show()
-    
-def listener():
+        middle_points.append(Point32(np.mean(x), np.mean(y), 0))
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    trajectory_polygon.polygon.points = middle_points
+    trajectory_polygon.header.frame_id = "map"
+    trajectory_polygon.header.stamp = rospy.get_rostime()
+    publisher.publish(trajectory_polygon)
 
+if __name__ == '__main__':
+
+    rospy.init_node('trajectory_finder', anonymous=True)
     rospy.Subscriber("chicken/map", Map, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-
-if __name__ == '__main__':
-    listener()
